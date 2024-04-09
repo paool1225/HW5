bb3c2d7309cf3c1e57d0913113dcb7eb962a2546
Paul Gomez <psgomez@usc.edu>
2024-04-09 10:28:51 +0000

    another new commit for hw5

--- schedwork.cpp
+++ schedwork.cpp
@@ -6,24 +6,47 @@
 #include <vector>
 #include <map>
 #include <algorithm>
-// add or remove necessary headers as you please
-
 #endif
-
 #include "schedwork.h"
-
 using namespace std;
 
-// a constant that can be used to indicate an INVALID 
-// worker ID if that is useful to your implementation.
-// Feel free to not use or delete.
 static const Worker_T INVALID_ID = (unsigned int)-1;
+bool scheduleHelp(
+    const AvailabilityMatrix& avail, 
+    DailySchedule& sched, 
+    const size_t dailyNeed, 
+    const size_t maxShifts, 
+    vector<size_t> rtn,  
+    size_t day, 
+    Worker_T worker)
+{
+    if(day == sched.size()) return true;
 
+    if(day == avail.size()-1) 
+        if(worker == dailyNeed) return true;
 
-// Add prototypes for any helper functions here
+    if(worker == dailyNeed)
+    {
+        day++;
+        worker = 0;
+    }
 
+    for(unsigned int i = 0; i < avail[0].size(); i++)
+    {
+        if(avail[day][i] && rtn[i] < maxShifts) 
+        {
+            sched[day][worker] = i;
+            rtn[i]++;
 
-// Add your implementation of schedule() and other helper functions here
+            if(scheduleHelp(avail, sched, dailyNeed, maxShifts, rtn, day, worker+1))
+                return true;
+
+            sched[day][worker] = INVALID_ID;
+            rtn[i]--;
+        }
+    }
+    return false;
+}
 
 bool schedule(
     const AvailabilityMatrix& avail,
@@ -36,10 +59,15 @@ bool schedule(
         return false;
     }
     sched.clear();
-    // Add your code below
-
-
-
-
+    for(unsigned int i = 0; i < avail.size(); i++)
+    {
+        vector<Worker_T> sched_;
+        for(unsigned int j = 0; j < dailyNeed; j++)
+        {
+            sched_.push_back(INVALID_ID);
+        }
+        sched.push_back(sched_);
+    }
+    vector<size_t> rtn(avail[0].size(), 0);  
+    return scheduleHelp(avail, sched, dailyNeed, maxShifts, rtn, 0, 0);
 }
-
--- wordle.cpp
+++ wordle.cpp
@@ -6,14 +6,17 @@
 #include <map>
 #include <set>
 #endif
-
 #include "wordle.h"
 #include "dict-eng.h"
 using namespace std;
 
+string alphabet = "abcdefghijklmnopqrstuvwxyz"; 
 
 // Add prototypes of helper functions here
-
+void combos(set<string> &return_set,
+    std::string in,
+    std::string floating,
+    const std::set<std::string>& dict, size_t location, std::string next, int number_of_any_letters);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -21,8 +24,54 @@ std::set<std::string> wordle(
     const std::string& floating,
     const std::set<std::string>& dict)
 {
-    // Add your code here
-
+    set<string> myset; 
+    string next = in; 
+    size_t number_of_dashes = 0; 
+    int number_of_any_letters = 0; 
+    for (size_t i = 0; i < in.size(); i++){
+        if (in[i] == '-') number_of_dashes++; 
+    }
+    number_of_any_letters = static_cast<int>(number_of_dashes) - static_cast<int>(floating.size()); 
+    combos(myset, in, floating, dict, 0, next, number_of_any_letters);
+    return myset; 
 }
 
-// Define any helper functions here
+void combos(set<string> &return_set,
+    std::string in,
+    std::string floating,
+    const std::set<std::string> &dict, size_t location, std::string next, int number_of_any_letters)
+{   
+    if (location == in.size()){
+        return; 
+    }
+    if (next[location] != '-'){
+        if (location == in.size()-1){
+            if (dict.find(next) != dict.end()) return_set.insert(next); 
+        }
+        size_t x = location + 1;
+        combos(return_set, in, floating, dict, x, next, number_of_any_letters); 
+    } else {
+        if (number_of_any_letters > 0){
+            for (size_t i = 0; i < alphabet.size(); i++){
+                next[location] = alphabet[i]; 
+                if (location == in.size()-1){
+                    if (dict.find(next) != dict.end()) return_set.insert(next); 
+                }
+                size_t x = location + 1; 
+                int z = number_of_any_letters - 1;
+                combos(return_set, in, floating, dict, x, next, z); 
+            }
+        }
+
+        for (size_t i = 0; i < floating.size(); i++){
+            string temp = floating; 
+            next[location] = temp[i]; 
+            temp.erase(i, 1); 
+            if (location == in.size()-1){
+                if (dict.find(next) != dict.end()) return_set.insert(next); 
+            }
+            size_t x = location + 1; 
+            combos(return_set, in, temp, dict, x, next, number_of_any_letters); 
+        }
+    }
+}
